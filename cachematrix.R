1e5121ad580ab00715238c5a7944920c0d82aa40
Manuel Simoes (Leonis) <mianuel@gmail.com>
2015-02-22 23:35:14 +0100

    Add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Thoses functions creates a special "matrix" object storing the matrix
+##	and its inverse (if calculated at least one time) and restore its inverse if
+##	futher re-calculation is asked.
+##
+##	The matrix should be store in a new object using the function makeCacheMatrix()
+##	as newMatrix <- makeCacheMatrix(matrix)
+## 	and inverse can be calculated or obtain using the function
+##	cacheSolve(newMatrix).
+##
+##
+## example:
+## --------
+## 	In this example we show a typical case of use of the functions 
+##		makeCacheMatrix() and cacheSolve()
+##
+## Here we build a square Matrix (x)
+##	
+##> x <- stats::rnorm(16)
+##> dim(x) <- c(4,4)
+##> x
+##[,1]       [,2]       [,3]       [,4]
+##[1,]  1.72217038  0.2740427 -0.4090343  0.2861305
+##[2,]  0.05641769 -0.3610035 -0.2356725  1.5717658
+##[3,] -1.84614573  1.3250853  0.9297387 -0.8355489
+##[4,]  0.22937111  0.1135067 -0.5113745 -0.1866716
+##
+## When build a new matrix object containing the matrix x.
+##
+##> newM <- makeCacheMatrix(x)
+##
+## For the first calculation of inverse of the matrix
+##	stored in newM, the inverse is calculated with the
+##	function solve.
+##
+##> cacheSolve(newM)
+##[,1]       [,2]        [,3]       [,4]
+##[1,] 0.5272583 -0.2265384 -0.12423007 -0.5432015
+##[2,] 0.6232713  0.3044524  0.66056120  0.5621300
+##[3,] 0.3123929 -0.2794786  0.03212953 -2.0181726
+##[4,] 0.1710680  0.6723799  0.16099455 -0.1539992
+##
+## The following time of the 
+##
+##> cacheSolve(newM)
+##getting cached data
+##[,1]       [,2]        [,3]       [,4]
+##[1,] 0.5272583 -0.2265384 -0.12423007 -0.5432015
+##[2,] 0.6232713  0.3044524  0.66056120  0.5621300
+##[3,] 0.3123929 -0.2794786  0.03212953 -2.0181726
+##[4,] 0.1710680  0.6723799  0.16099455 -0.1539992
 
-## Write a short comment describing this function
+
+
+## This function creates a special "matrix" object that store
+##	the matrix giving as argument and its inverse if calculate
+##	with cacheSolve.
+##
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
@@ -19,6 +72,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+##	(if calculated at least one time) and restore its inverse (if
+##	re-calculated is more than one time).
 
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
